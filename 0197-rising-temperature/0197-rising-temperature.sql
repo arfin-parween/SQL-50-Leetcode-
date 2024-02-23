798d7c5dbe4b3fcdb3aacd4223027e219006725a
Arfin Parween <wbstartpracticing@gmail.com>
2024-02-24 01:02:56 +0530

    Time: 578 ms (98.03%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -8,8 +8,18 @@ from Weather w1
 join Weather w2
 where datediff(w1.recordDate,w2.recordDate)=1 
 and w1.temperature > w2.temperature;*/
-select w1.id 
+/*select w1.id 
 from Weather w1
 join Weather w2
 on datediff(w1.recordDate,w2.recordDate)=1 
+and w1.temperature > w2.temperature;*/
+/*select w1.id 
+from Weather w1
+join Weather w2
+where subdate(w1.recordDate,1)=w2.recordDate
+and w1.temperature > w2.temperature;*/
+select w1.id 
+from Weather w1
+join Weather w2
+on subdate(w1.recordDate,1)=w2.recordDate
 and w1.temperature > w2.temperature;
